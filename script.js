9d1b4a93ea7ab96ce12d0c0f751829909a159997
4aaronn <falledgottermed@gmail.com>
2026-03-21 05:20:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-async function downloadFile(file) {
-  try {
-    const res = await fetch(`/api/generate?file=${file}`);
-    const data = await res.json();
+async function loadFiles() {
+  const res = await fetch("/api/list");
+  const data = await res.json();
 
-    // iOS-like smooth redirect
-    window.location.href = data.url;
+  const container = document.querySelector(".container");
 
-  } catch (err) {
-    alert("Download failed");
-  }
+  data.files.forEach(file => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerText = "📄 " + file;
+
+    div.onclick = () => downloadFile(file);
+
+    container.appendChild(div);
+  });
 }
+
+async function downloadFile(file) {
+  const res = await fetch(`/api/generate?file=${file}`);
+  const data = await res.json();
+
+  window.location.href = data.url;
+}
+
+loadFiles();
